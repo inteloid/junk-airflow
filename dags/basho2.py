706b18b18ae708d393c13543beedadc66ad52459
inteloid <harut.martirosyan@gmail.com>
2023-01-09 15:42:58 +0400

    initv2

---
+++ dags/basho2.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+	"basho2",
+	default_args={
+	        "depends_on_past": False,
+	        "email": ["airflow@example.com"],
+	        "email_on_failure": False,
+	        "email_on_retry": False,
+	        "retries": 1,
+	        "retry_delay": timedelta(minutes=5),
+	        # 'queue': 'bash_queue',
+	        # 'pool': 'backfill',
+	        # 'priority_weight': 10,
+	        # 'end_date': datetime(2016, 1, 1),
+	        # 'wait_for_downstream': False,
+	        # 'sla': timedelta(hours=2),
+	        # 'execution_timeout': timedelta(seconds=300),
+	        # 'on_failure_callback': some_function,
+	        # 'on_success_callback': some_other_function,
+	        # 'on_retry_callback': another_function,
+	        # 'sla_miss_callback': yet_another_function,
+	        # 'trigger_rule': 'all_success'
+	},
+	description="A simple tutorial DAG",
+	schedule=timedelta(days=1),
+	start_date=datetime(2021, 1, 1),
+	catchup=False,
+	tags=["example"],
+) as dag:
+	run_this = BashOperator(
+	    task_id="run_after_loop",
+	    bash_command="echo 1",
+	)
