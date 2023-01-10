f1f0c49385bc6af5efbfc8d95ada5333ae5651a3
Harut Martirosyan <harut.martirosyan@gmail.com>
2023-01-11 03:53:55 +0400

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,6 +1,7 @@
 from pyspark.sql.utils import AnalysisException
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+import random
 
 spark = SparkSession.builder.appName('job-for-pi').getOrCreate()
 NUM_SAMPLES = 10000
@@ -9,6 +10,6 @@ def inside(p):
     x, y = random.random(), random.random()
     return x*x + y*y < 1
 
-count = spark.parallelize(range(0, NUM_SAMPLES)) \
+count = spark.sparkContext.parallelize(range(0, NUM_SAMPLES)) \
              .filter(inside).count()
 print("Pi is roughly %f" % (4.0 * count / NUM_SAMPLES))
