f67a92affded9b0249c3c5c3b8ca0099e15c1c31
Harut Martirosyan <harut.martirosyan@gmail.com>
2023-02-02 22:47:17 +0400

    Update testoperator.py

--- testoperator.py
+++ testoperator.py
@@ -1,8 +1,24 @@
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator 
+import sys
+import json
+
 
 class TestOperator(SparkSubmitOperator):
-    def __init__(self, name: str, **kwargs) -> None:
+    def __init__(self, name: str, notebook: str, **kwargs) -> None:
+        convertNotebook(notebook)
         super().__init__(**kwargs)
 
     def execute(self, context):
         super().execute(context)
+        
+    def convertNotebook(notebook)
+        print('# file: %s' % notebook)
+        print('# vi: filetype=python')
+        print('')
+        code = json.load(open(notebook))
+
+        for cell in code['cells']:
+                # print('# -------- code --------', cell['cell_type'])
+                for line in cell['source']:
+                    print(line, end='')
+                print('\n')
