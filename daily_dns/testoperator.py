b02d0e895ca195b6d1de9470230c44d74dd3292e
Harut Martirosyan <harut.martirosyan@gmail.com>
2023-02-04 20:46:42 +0400

    indentation

--- daily_dns/testoperator.py
+++ daily_dns/testoperator.py
@@ -19,7 +19,7 @@ class TestOperator(SparkSubmitOperator):
 
         pyFileContent = """from pyspark.sql import SparkSession; \n
 spark = SparkSession.builder.appName('job-for-pi').getOrCreate(); \n
-                        """
+"""
         for cell in code['cells']:
                 # print('# -------- code --------', cell['cell_type'])
                 for line in cell['source']:
