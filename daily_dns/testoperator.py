abcc1f206ca9ac2bbf698a097c44394213258b46
Harut Martirosyan <harut.martirosyan@gmail.com>
2023-02-05 14:32:01 +0400

    Update testoperator.py

--- daily_dns/testoperator.py
+++ daily_dns/testoperator.py
@@ -24,8 +24,8 @@ class TestOperator(SparkSubmitOperator):
             'spark.hadoop.fs.s3a.path.style.access': 'true',
             'spark.hadoop.fs.s3a.connection.ssl.enabled': 'false',
             'spark.hadoop.fs.s3a.impl': 'org.apache.hadoop.fs.s3a.S3AFileSystem',
-            'spark.executor.instances': 4,
-            'spark.executor.cores': 2,
+            'spark.executor.instances': 6,
+            'spark.executor.cores': 4,
             'spark.executor.memory': "16g",
             'spark.rpc.askTimeout': 36000
         };
